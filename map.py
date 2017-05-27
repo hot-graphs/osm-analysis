88eada36e671dc71c3574f51cd4d7f40fd4cbbea
Petr Viktorin <encukou@gmail.com>
2017-05-27 15:53:56 +0200

    Add visualization of neighbor areas

--- map.py
+++ map.py
@@ -10,7 +10,7 @@ from kivy.properties import NumericProperty, ObjectProperty, ListProperty, \
 import os.path
 import pandas
 
-points = pandas.read_csv('points.csv').set_index(['GPS lat', 'GPS lon'])
+points = pandas.read_csv('points.csv').set_index(['GPS lat', 'GPS lon'])[-2:]
 
 
 class CustomMapView(MapView):
@@ -31,9 +31,18 @@ class CustomMapMarker(MapMarker):
         self.size = self.radius * 2, self.radius * 2
 
         self.canvas.clear()
+        with self.canvas:
+            graphics.PushMatrix()
+            self.translation_pre = graphics.Translate(0, 0)
+            graphics.Color(1, 0, 0, 0.15)
+            self.radius_circles = [graphics.Ellipse() for i in row]
+            graphics.PopMatrix()
+
         with self.canvas.after:
             graphics.PushMatrix()
             self.translation = graphics.Translate(0, 0)
+
+
             print(row)
             for pos, value in enumerate(reversed(row)):
                 rpos = (len(row) - pos) / len(row)
@@ -49,12 +58,25 @@ class CustomMapMarker(MapMarker):
         self.bind(pos=self.reposition)
 
     def reposition(self, *args):
+        self.translation_pre.xy = self.pos
         self.translation.xy = self.pos
+        mapview = self
+        while not isinstance(mapview, CustomMapView):
+            mapview = mapview.parent
+            if mapview is None:
+                return
+        for i, (r, circle) in enumerate(zip(self.row.index, self.radius_circles)):
+            r = float(r)
+            xm, ym, = mapview.get_window_xy_from(self.lat, self.lon, zoom=mapview.zoom)
+            x1, y1 = mapview.get_window_xy_from(self.lat - r, self.lon - r, zoom=mapview.zoom)
+            x2, y2 = mapview.get_window_xy_from(self.lat + r, self.lon + r, zoom=mapview.zoom)
+            circle.pos = x1 - xm, y1 - ym
+            circle.size = x2-x1, y2-y1
 
 
 class MapViewApp(App):
     def build(self):
-        self.mapview = CustomMapView(zoom=18, lat=49.213795607926734, lon=16.58214582519531)
+        self.mapview = CustomMapView(zoom=18, lat=49.22025828658787, lon=16.50821292434091) #49.213795607926734, lon=16.58214582519531)
         self.points_iter = iter(points.iterrows())
         self.add_next_points()
         self.bind(on_touch_down=print)
